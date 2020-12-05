90d38d2392308297648413518d5a73e320bb69c3
Thomas Mayer <strassertom@gmx.de>
2020-12-05 23:08:48 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,65 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"lg-ess-home adapter settings": {
+		"en": "Adapter settings for lg-ess-home",
+		"de": "Adaptereinstellungen für lg-ess-home",
+		"ru": "Настройки адаптера для lg-ess-home",
+		"pt": "Configurações do adaptador para lg-ess-home",
+		"nl": "Adapterinstellingen voor lg-ess-home",
+		"fr": "Paramètres d'adaptateur pour lg-ess-home",
+		"it": "Impostazioni dell'adattatore per lg-ess-home",
+		"es": "Ajustes del adaptador para lg-ess-home",
+		"pl": "Ustawienia adaptera dla lg-ess-home",
+		"zh-cn": "lg-ess-home的适配器设置"
+	},
+	"IP Adress": {
+		"en": "IP Adress",
+		"de": "IP Adresse",
+		"ru": "IP адрес",
+		"pt": "Endereço de IP",
+		"nl": "IP adres",
+		"fr": "Adresse IP",
+		"it": "Indirizzo IP",
+		"es": "Dirección IP",
+		"pl": "Adres IP",
+		"zh-cn": "IP地址"
+	},
+	"Password": {
+		"en": "Password",
+		"de": "Passwort",
+		"ru": "пароль",
+		"pt": "Senha",
+		"nl": "Wachtwoord",
+		"fr": "Mot de passe",
+		"it": "Parola d'ordine",
+		"es": "Contraseña",
+		"pl": "Hasło",
+		"zh-cn": "密码"
+	},
+	"Update intervall [s]": {
+		"en": "Update intervall [s]",
+		"de": "Update Intervall [s]",
+		"ru": "интервал обновления [с]",
+		"pt": "atualizar intervalo [s]",
+		"nl": "update interval [s]",
+		"fr": "mise à jour de l'intervalle [s]",
+		"it": "aggiornamento intervall [s]",
+		"es": "actualizar intervalo [s]",
+		"pl": "aktualizacja interwału [s]",
+		"zh-cn": "更新时间间隔[s]"
+	},
+	"Update String data in short intervall": {
+		"en": "Update String data in short intervall",
+		"de": "Aktualisieren der String-Daten in kurzen Intervall",
+		"ru": "Обновление строковых данных за короткий интервал",
+		"pt": "Atualizar os dados da string em um curto intervalo",
+		"nl": "Update String-gegevens met korte tussenpozen",
+		"fr": "Mettre à jour les données de chaîne dans un bref intervalle",
+		"it": "Aggiorna i dati della stringa a brevi intervalli",
+		"es": "Actualizar datos de cadenas en un intervalo corto",
+		"pl": "Aktualizuj dane String w krótkich odstępach czasu",
+		"zh-cn": "在短间隔内更新字符串数据"
+	}
+};
